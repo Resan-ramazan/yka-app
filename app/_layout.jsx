c7d3be34021de39c8098f31a41b2084d64af4634
Ramazan Alkoyun <ramazan.resan@gmail.com>
2024-06-19 00:23:47 +0200

    Adding a token cache

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,10 +3,30 @@ import { View, Text } from "react-native";
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import LoginScreen from '../components/LoginScreen'
 
+import * as SecureStore from "expo-secure-store";
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
 export default function RootLayout() {
   return (
     <ClerkProvider
       publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      tokenCache={tokenCache}
     >
       <SignedIn>
         <Stack screenOptions={{ headerShown: false }}>
